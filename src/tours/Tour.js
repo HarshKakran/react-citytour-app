1099685e40ae58959107082a53582dffd548007f
HarshKakran <harshkakran05@gamil.com>
2022-01-18 12:21:43 +0530

    Added images

--- src/tours/Tour.js
+++ src/tours/Tour.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 
 function Tours(props) {
-  console.log(props);
   const [readMore, setReadMore] = useState(false);
   const { tours, fetchTours } = props;
-  console.log(tours);
   const [tourData, setTourData] = useState(tours);
-  console.log(`TourData: ${tourData}`);
 
   const removeTour = (id) => {
     let newTourData = tourData.filter((tour) => tour.id !== id);
@@ -15,7 +12,6 @@ function Tours(props) {
   if (tourData.length === 0) {
     return (
       <main>
-        {console.log("length=0")}
         <div className="title">
           <h2>no tours left</h2>
           <button className="btn" onClick={() => fetchTours()}>
@@ -29,12 +25,11 @@ function Tours(props) {
     <>
       <h2>Our Tours</h2>
       {tourData.map((tour) => {
-        // console.log(tour);
-        const { id, img, info, name, price } = tour;
-        console.log(`${id}`);
+        console.log(tour);
+        const { id, image, info, name, price } = tour;
         return (
           <article key={id} className="single-tour">
-            <img src={img} alt={name} />
+            <img src={image} alt={name} />
             <footer>
               <div className="tour-info">
                 <h4>{name}</h4>
